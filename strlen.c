9279212a19d99cc48490c232a860cfd8d49fb87c
sanggoe <sanggoe0509@gmail.com>
2021-08-12 18:32:06 +0900

    7'th week study

--- strlen.c
+++ strlen.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
-#include <string.h>
+
+void myStrcat(char* to, char* from);
 
 void main() {
+	char to[21] = "I'm ";
+	char* from = "kyeong jong!";
 
+	myStrcat(to, from);
+	printf("%s\n", to);
+}
+
+void myStrcat(char* to, char* from) {
+
+	while (*(to++) != '\0');
+	to--;
+
+	while (*(from) != '\0') {
+		*(to++) = *(from++);
+
+		// to[i] = from[i];
+		// i++;
+	}
+	*to = '\0';
 }
